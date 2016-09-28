eed371d356ba2adf707bfb77cee1f2ae122085ea
DiscoverSDK <liran@lirans-MacBook-Pro.local>
2016-09-28 08:11:25 +0300

    all

---
+++ core.mk
@@ -0,0 +1,57 @@
+#
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Base configuration for communication-oriented android devices
+# (phones, tablets, etc.).  If you want a change to apply to ALL
+# devices (including non-phones and non-tablets), modify
+# core_minimal.mk instead.
+
+PRODUCT_PACKAGES += \
+    BasicDreams \
+    cmyapp \
+    libNativeService \
+    serverapp \
+    libSampClient \
+    clientapp \
+    libhelloworld_service \
+    helloworld_client \
+    helloworld_server \
+    test111 \
+    com.android.fpgaservice \
+    com.android.fpgaservice.xml \
+    FpgaServiceApp \
+    libfpga_jni \
+    myfpga.default \
+    fpgatestapp \
+    com.android.sampservice \
+    com.android.sampservice.xml \
+    SampServiceApp \
+    com.android.fibservice \
+    com.android.fibservice.xml \
+    FibServiceApp \
+    libfib_jni \
+    Contacts \
+    DocumentsUI \
+    DownloadProviderUi \
+    ExternalStorageProvider \
+    KeyChain \
+    PicoTts \
+    PacProcessor \
+    ProxyHandler \
+    SharedStorageBackup \
+    VpnDialogs
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_base.mk)
